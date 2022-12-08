e8db0f66a6d7803e90ed73f84fc51e9a970f6eae
KerileeCar <88647315+KerileeCar@users.noreply.github.com>
2022-12-08 13:36:50 +0200

    Upload Project

--- README.md
+++ README.md
@@ -1,18 +1,10 @@
-# Oh-My-Wordle
-The aim of this project was to create a multiplayer version of the popular Wordle game.
+# 2022-group-lab-007
 
-Play it now at "https://ohmywordle.azurewebsites.net". A video demonstration of the application can be downloaded from the v1.0 release.
+Get ready for "Oh my Wordle!".
 
-Game Preview Pictures:
-
-<img width="722" alt="image" src="https://user-images.githubusercontent.com/87419112/203595330-dfa097d0-1178-4028-875f-660681368494.png">
-
-
-/////////////////////////////////////////////////////////
+Play it now at "https://ohmywordle.azurewebsites.net"
 
 DEVELOPERS: 
-
-/////////////////////////////////////////////////////////
 
 -Tristan de Groot
             
@@ -23,29 +15,3 @@ DEVELOPERS:
 -Mohammed Raees Haffejee
             
 -Zunaid Valodia
-
-/////////////////////////////////////////////////////////
-
-FEATURES:
-
-/////////////////////////////////////////////////////////
-
--Created as a Web application
-
--Single player mode
-
--Multiplayer Mode
-
--Custom Multiplayer Mode (Where a third player sets a word)
-
--A login/account system using a secure database server.
-
--Additional pages such as a scoreboard.
-
-/////////////////////////////////////////////////////////
-
-DEV INFORMATION:
-
-/////////////////////////////////////////////////////////
-
--Implemented using the scrumboard procedure and sprints (See documentation)
---
+++ index.js
@@ -0,0 +1,164 @@
+'use strict'
+
+// ///////////////////////////////////////////////////
+// imports and configuration files
+// ///////////////////////////////////////////////////
+// Express framework for routing
+const express = require('express')
+const path = require('path')
+// http for local server connection
+const http = require('http')
+// defining a port
+const PORT = process.env.PORT || 3003
+// socket instance creation
+const socketio = require('socket.io')
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+// session for user token
+const session = require('express-session');
+const FileStore = require('session-file-store')(session)
+// server configuration
+const db = require('./serverConfig.js')
+
+// ///////////////////////////////////////////////////
+// user token session
+// ///////////////////////////////////////////////////
+app.use(session({
+  store: new FileStore(),
+  secret: 'lacrazywordgamesqwertyasdfa',
+  resave: false,
+  saveUninitialized: false
+})
+)
+
+// ///////////////////////////////////////////////////
+// Socket setup
+// ///////////////////////////////////////////////////
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Start server
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// Handle a socket connection request from web client
+const connections = [null, null] //edit here for allowing an addition player
+
+let NumClientA=0
+let NumClientB=0
+let roomNo=" "
+let roomsize
+let playernum
+let playernumB
+
+io.on('connection', socket => {
+
+  socket.on('ConnectedA',()=>{
+    NumClientA++;
+    playernum=NumClientA%2
+  //Sets 2 clients to a single room
+    roomNo = Math.round(NumClientA / 2)+"A";
+    socket.join(roomNo);
+    console.log(`New client no.: ${NumClientA}, room no.: ${roomNo}`);
+    socket.emit('serverMsg',roomNo,playernum)
+  })
+
+  socket.on('ConnectedB',()=>{
+    NumClientB++;
+
+  //Sets 3 clients to a single room
+    if(NumClientB%3==1){
+    roomNo = Math.round((NumClientB+1) / 3)+"B";
+    }else{
+      roomNo = Math.round(NumClientB / 3)+"B";
+    }
+    playernumB=NumClientB%3
+    socket.join(roomNo);
+    console.log(`New client no.: ${NumClientB}, room no.: ${roomNo}`);
+    socket.emit('serverMsg',roomNo,playernumB)
+
+  })
+
+    //Returns the number of clients in the room
+    socket.on('Checkroomsize',data=>{
+      roomsize=io.sockets.adapter.rooms.get(data).size
+      io.to(data).emit('Roomsize',roomsize)
+    })
+
+    //If matchmaking fails then client number is reduced so room can be used later
+    socket.on('Failed to matchmakeA',()=>{
+      NumClientA--;
+      socket.disconnect()
+    })
+
+    socket.on('Failed to matchmakeB',()=>{
+      NumClientB--;
+      socket.disconnect()
+    })
+
+    socket.on('SubmitWord',(clientroom,word,guessTile)=>{
+      socket.to(clientroom).emit('Sentword',word,guessTile)
+    })
+
+    socket.on('SetWordCustom',(clientroom,word,)=>{
+      socket.to(clientroom).emit('CustomWordSent',word)
+    })
+
+    socket.on('SetWordSend',(answer,room)=>{
+      io.to(room).emit('SetWordReceive',answer)
+    
+    })
+
+    // Check player connections
+  
+})
+
+
+// ///////////////////////////////////////////////////
+// settings for route use
+// ///////////////////////////////////////////////////
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.set('view engine', 'ejs')
+app.use(logger)
+
+// ///////////////////////////////////////////////////
+// set route paths
+// ///////////////////////////////////////////////////
+const createRouter = require('./routes/create')
+const loginRouter = require('./routes/users')
+const homeRouter = require('./routes/home')
+const InstructionsRouter = require('./routes/Instruction')
+const SingleGameRouter = require('./routes/SinglePlayer')
+const aboutRouter = require('./routes/about')
+const queueRouter=require('./routes/queue')
+const Multiplayer1Router = require('./routes/MultiplayerMode1')
+const Multiplayer2Router = require('./routes/MultiplayerMode2')
+const scoreboardRouter=require('./routes/scoreboard')
+const queueBRouter=require('./routes/queueB')
+
+
+// ///////////////////////////////////////////////////
+// Define routes
+// ///////////////////////////////////////////////////
+app.use('/create', createRouter)
+app.use('/users', loginRouter)
+app.use('/home', homeRouter)
+app.use('/instructions', InstructionsRouter)
+app.use('/singleplayer',SingleGameRouter)
+app.use('/about',aboutRouter)
+app.use('/queue',queueRouter)
+app.use('/MultiplayerMode1',Multiplayer1Router)
+app.use('/MultiplayerMode2',Multiplayer2Router)
+app.use('/scoreboard',scoreboardRouter)
+app.use('/queueB',queueBRouter)
+// ///////////////////////////////////////////////////
+// define function
+// ///////////////////////////////////////////////////
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
+
+module.exports = app
